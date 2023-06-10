2758df002ce947ff50dd8ac4636c19765a36430f
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-06-10 13:59:05 +0300

    Add UI to the game
    Add three buttons to allow the user to make a choice by clicking on
    them.
    Remove the getUserChoice function since it is no longer needed.
    Add appropriate tags to display the result of each round and the
    current score.
    Replace the use of alert to show the result by displaying it in the
    DOM.
    Refactor the game function and attach a click event to the buttons to
    call the game function when the user clicks on any of the buttons.

--- src/script.js
+++ src/script.js
@@ -1,42 +1,42 @@
-game()
+const gameOptions = document.querySelectorAll("button");
+const result = document.querySelector("#results");
+const userScoreMessage = document.querySelector("#user-score");
+const computerScoreMessage = document.querySelector("#computer-score");
+let userScore = 0;
+let computerScore = 0;
 
-function game() {
-    let userScore = 0;
-    let computerScore = 0;
-    let winner;
-    let runNewRound = true;
-    while (runNewRound) {
-        let roundWinner = playRound();
-        switch (roundWinner) {
-            case "user":
-                userScore++;
-            break;
-            case "computer":
-                computerScore++;
-            break;
-        }
-        runNewRound = userScore < 5 && computerScore < 5;
+gameOptions.forEach(option => {
+    option.addEventListener("click", game);
+});
+
+function game(event) {
+    const userChoice = event.target.getAttribute("data-option");
+    let roundWinner = playRound(userChoice);
+
+    if (roundWinner === "user") {
+        userScore++;
+        userScoreMessage.textContent = `User score - ${userScore}`;
+    } else {
+        computerScore++;
+        computerScoreMessage.textContent = `Computer score - ${computerScore}`;
     }
 
     if (userScore === 5) {
-        winner = "User";
-    } else {
-        winner = "Computer";
+        alert("You won!");
+    } else if (computerScore === 5) {
+        alert("You lose!");
     }
-
-    return winner;
 }
 
-function playRound() {
+function playRound(userChoice) {
     const computerChoice = getComputerChoice();
-    const userChoice = getUserChoice();
     let winner = checkWinner(userChoice, computerChoice);
     let message = 
         winner === "user" ? `You Won! ${userChoice} beats ${computerChoice}` :
         winner === "computer" ? `You Lose! ${computerChoice} beats ${userChoice}` :
         `Draw ${userChoice} is equal ${computerChoice}`
     
-    alert(message);
+    result.textContent = message;
     return winner;
 }
 
@@ -54,23 +54,8 @@ function checkWinner(userChoice, computerChoice) {
     ) {
         return "computer";
     } else {
-        return "Draw";
+        return "draw";
     }
-}
-
-function getUserChoice() {
-    const userChoice = prompt("Rock, Paper or Scissors?", "");
-    const lowerCaseChoice = userChoice.toLowerCase();
-    if (
-        lowerCaseChoice === "rock" ||
-        lowerCaseChoice === "paper" ||
-        lowerCaseChoice === "scissors"
-    ) {
-        return lowerCaseChoice;
-    }
-
-    alert("You need to write: rock, paper or scissors, try again!");
-    getUserChoice();
 }
 
 function getComputerChoice() {
@@ -87,35 +72,3 @@ function getComputerChoice() {
 
     return computerChoice;
 }
-
-/* 
-    Write a getComputerChoice() function which makes computer's choice:
-        create a randomNum variable which will contain a random number from 1 to 3,
-        create a computerChoice variable which will contain a result of computer's choice,
-        use condition to assign the appropriate result to computerChoice variable:
-            randomNum = 1 - rock,
-            randomNum = 2 - paper,
-            randomNum = 3 - scissors;
-        output the computerChoice value;
-    write a getUserChoice() function which allows a user to take its choice:
-        ask for user input and assign the result to userChoice variable,
-        convert userChoice to case insensitive,
-        check if user wrote one of 3 key words if it's so output the result, else alert message and try again;
-    write a playRound function which runs one round of the game and output the winner:
-        create a computerChoice variable to hold computer's choice,
-        create a userChoice variable to hold user's choice,
-        create a message variable to inform whether user win or lose,
-        create a winner variable to hold winner,
-        create condition which check who won and assign values to message and winner,
-        show message,
-        output the winner variable;
-    write a game() function which runs game while one side won't get 5 scores:
-        create userScore variable to hold user score,
-        create computerScore variable to hold computer score,
-        create variable winner,
-        create variable runNewRound to iterate while it is true,
-        iterate while one of those variables don't reach 5,
-        assign to winner variable current winner;
-    write a gameOver() function;
-    write a newGame() function;
-*/
